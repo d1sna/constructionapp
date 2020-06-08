d912f9ba9572fec3862535ee2cafaeefcd5b771e
Alexandr <sanyad1sna@gmail.com>
2020-06-08 12:03:46 +0300

    some fix

--- routes/main.js
+++ routes/main.js
@@ -2,19 +2,11 @@ const {
     Router
 } = require('express'),
     router = Router(),
-    // mongoose = require('mongoose'),
     bodyParser = require("body-parser"),
     urlencodedParser = bodyParser.urlencoded({
         extended: false
     }),
     Object = require('../models/object');
-
-// mongoose.connect(
-//     'mongodb+srv://d1sna:89636579208@cluster0-wuwpp.mongodb.net/objects', {
-//         useNewUrlParser: true,
-//         useFindAndModify: false,
-//         useUnifiedTopology: true
-//     });
 
 router.get('/', (req, res) => {
     res.render('index', {
